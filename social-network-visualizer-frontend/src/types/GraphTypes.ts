2e2c5c52a0514da06c495c5e7445969e48d0bc80
Wiktor Dybalski <wdybalski@student.agh.edu.pl>
2025-10-06 22:52:53 +0200

    First Improvements of graph visuzalizations

--- social-network-visualizer-frontend/src/types/GraphTypes.ts
+++ social-network-visualizer-frontend/src/types/GraphTypes.ts
@@ -1,15 +1,61 @@
 import { NodeObject, LinkObject } from 'force-graph';
 
+export interface AuthorNode extends NodeObject {
+    nodeType: NodeType.AUTHOR;
+    degreeCentrality: number;
+    pagerank: number;
+    community: string;
+}
+export interface TweetNode extends NodeObject {
+    nodeType: NodeType.TWEET;
+}
+export interface HashtagNode extends NodeObject {
+    nodeType: NodeType.HASHTAG;
+}
+
+export enum NodeType {
+    AUTHOR = 'AUTHOR',
+    TWEET = 'TWEET',
+    HASHTAG = 'HASHTAG'
+}
+export type GraphNode = AuthorNode | TweetNode | HashtagNode;
+
+export interface PostedLink extends LinkObject {
+    linkType: LinkType.POSTED;
+}
+export interface MentionsLink extends LinkObject {
+    linkType: LinkType.MENTIONS;
+}
+export interface RepliedToLink extends LinkObject {
+    linkType: LinkType.REPLIED_TO;
+}
+export interface RetweetLink extends LinkObject {
+    linkType: LinkType.RETWEET;
+}
+export interface HasHashtagLink extends LinkObject {
+    linkType: LinkType.HAS_HASHTAG;
+}
+
+export enum LinkType {
+    POSTED = 'POSTED',
+    MENTIONS = 'MENTIONS',
+    REPLIED_TO = 'REPLIED_TO',
+    RETWEET = 'RETWEET',
+    HAS_HASHTAG = 'HAS_HASHTAG'
+}
+export type GraphLink = PostedLink | MentionsLink | RepliedToLink | RetweetLink | HasHashtagLink;
+
+
 export interface GraphProps {
     graphData: {
-        nodes: NodeObject[];
-        links: LinkObject[];
+        nodes: GraphNode[];
+        links: GraphLink[];
     };
-    nodeVal: (node: NodeObject) => number;
-    nodeLabel: (node: NodeObject) => string;
-    nodeColor: (node: NodeObject) => string;
-    linkColor: (link: LinkObject) => string;
-    linkWidth: (link: LinkObject) => number;
+    nodeVal: (node: GraphNode) => number;
+    nodeLabel: (node: GraphNode) => string;
+    nodeColor: (node: GraphNode) => string;
+    linkColor: (link: GraphLink) => string;
+    linkWidth: (link: GraphLink) => number;
     linkDirectionalArrowLength: number;
     linkDirectionalArrowRelPos: number;
     nodeFoundId: string | null;
