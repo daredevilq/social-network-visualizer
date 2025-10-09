b16e2d28bcd17f131322ef3f6a826b3198c629ed
Wiktor Dybalski <wdybalski@student.agh.edu.pl>
2025-10-09 22:54:57 +0200

    Add server side changes for the new graph implementation

--- social-network-visualizer-backend/src/main/java/com/example/social_network_visualizer_backend/controller/GraphController.java
+++ social-network-visualizer-backend/src/main/java/com/example/social_network_visualizer_backend/controller/GraphController.java
@@ -1,13 +1,17 @@
 package com.example.social_network_visualizer_backend.controller;
 
 import com.example.social_network_visualizer_backend.dto.BridgeDto;
-import com.example.social_network_visualizer_backend.dto.GraphDataDto;
 import com.example.social_network_visualizer_backend.dto.GraphTypeDto;
+import com.example.social_network_visualizer_backend.dto.graph.GraphDataDto;
 import com.example.social_network_visualizer_backend.service.GraphService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
--- social-network-visualizer-backend/src/main/java/com/example/social_network_visualizer_backend/dto/AuthorNodeDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.social_network_visualizer_backend.dto;
-
-public record AuthorNodeDto(
-        String name,
-        Double pagerank,
-        Double centrality,
-        Integer community
-) {
-}
