c08f1494aa129cf9a87e206307928453c6385cf7
TommyFurgi <tomaszfurgala23@gmail.com>
2025-10-18 10:21:57 +0200

    add context composer

--- social-network-visualizer-frontend/src/app/layout.tsx
+++ social-network-visualizer-frontend/src/app/layout.tsx
@@ -1,24 +1,15 @@
 'use client';
 
 import './globals.css'
-import { ProjectProvider } from './context/ProjectContext'
-import {NotificationProvider} from "@/app/context/NotificationProvider";
-import {WorkspaceProvider} from "@/app/context/WorkspaceContext";
-import {GraphProvider} from "@/app/context/GraphContext";
+import {AppProviders} from "@/app/providers/AppComposers";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<body suppressHydrationWarning>
-				<NotificationProvider>
-					<ProjectProvider>
-						<WorkspaceProvider>
-							<GraphProvider>
-								{children}
-							</GraphProvider>
-						</WorkspaceProvider>
-					</ProjectProvider>
-				</NotificationProvider>
+				<AppProviders>
+					{children}
+				</AppProviders>
 			</body>
 		</html>
 	);
---
+++ social-network-visualizer-frontend/src/app/providers/ProviderComposer.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import React from 'react';
+
+type ProviderProps = {
+    children: React.ReactNode;
+};
+
+type ComposerProps = {
+    providers: Array<React.ComponentType<ProviderProps>>;
+    children: React.ReactNode;
+};
+
+export const ProviderComposer = ({ providers, children }: ComposerProps) => {
+    return (
+        <>
+            {providers.reduceRight((acc, Provider) => {
+                return <Provider>{acc}</Provider>;
+            }, children)}
+        </>
+    );
+};
