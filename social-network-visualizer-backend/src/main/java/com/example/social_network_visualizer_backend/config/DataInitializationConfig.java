951fdf9f92e1125966151b98951f0ad3be66853f
Wiktor Dybalski <wdybalski@student.agh.edu.pl>
2025-10-21 15:35:12 +0200

    add test profile to provide test passes

--- social-network-visualizer-backend/src/main/java/com/example/social_network_visualizer_backend/config/DataInitializationConfig.java
+++ social-network-visualizer-backend/src/main/java/com/example/social_network_visualizer_backend/config/DataInitializationConfig.java
@@ -8,9 +8,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Slf4j
 @Configuration
+@Profile("!test")
 @RequiredArgsConstructor
 public class DataInitializationConfig {
   private final MongodbService mongodbService;
