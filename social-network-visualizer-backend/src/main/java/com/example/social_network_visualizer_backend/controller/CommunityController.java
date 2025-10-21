2dff5689142e934a0078d6dcbab11234a26627b2
Wiktor Dybalski <wdybalski@student.agh.edu.pl>
2025-10-21 21:38:46 +0200

    add test log

--- social-network-visualizer-backend/src/main/java/com/example/social_network_visualizer_backend/controller/CommunityController.java
+++ social-network-visualizer-backend/src/main/java/com/example/social_network_visualizer_backend/controller/CommunityController.java
@@ -25,6 +25,7 @@ public class CommunityController {
   @GetMapping("/list-slow")
   public ResponseEntity<List<CommunitySummary>> getCommunities(
       @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "25") int size) {
+    System.out.println("cos tam");
 
     List<CommunitySummary> slice = communityService.listCommunities(page, size);
     return ResponseEntity.ok(slice);
