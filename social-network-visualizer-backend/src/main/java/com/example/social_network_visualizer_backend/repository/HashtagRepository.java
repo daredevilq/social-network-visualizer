79fc05d90b7eb0bc02b71974b00c8c18b434b237
TommyFurgi <tomaszfurgala23@gmail.com>
2025-10-22 22:22:47 +0200

    fix displaying hashtags on graph

--- social-network-visualizer-backend/src/main/java/com/example/social_network_visualizer_backend/repository/HashtagRepository.java
+++ social-network-visualizer-backend/src/main/java/com/example/social_network_visualizer_backend/repository/HashtagRepository.java
@@ -35,7 +35,7 @@ public interface HashtagRepository extends Neo4jRepository<Hashtag, String> {
                 MATCH (h:Hashtag)
                 WHERE $inWorkspace = false OR h.isInWorkspace = true
                 RETURN
-                h.hashtag AS name,
+                h.hashtag AS id,
                 'HASHTAG' AS nodeType
                 ORDER BY h.hashtag
             """)
