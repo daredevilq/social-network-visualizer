5060015f00c6d14564a6933fb0750d6dc4c43b90
daredevilq <110993908+daredevilq@users.noreply.github.com>
2025-10-24 00:41:47 +0200

    comments cleanup

--- social-network-visualizer-frontend/src/app/utils/nodeRelationMap.ts
+++ social-network-visualizer-frontend/src/app/utils/nodeRelationMap.ts
@@ -1,29 +1,19 @@
 import { NodeType, RelationType } from "@/types/GraphTypes";
 
 export const RELATION_NODE_MAP: Record<RelationType, [NodeType, NodeType]> = {
-  // AUTHOR → AUTHOR relations
+
   [RelationType.MENTIONS]: [NodeType.AUTHOR, NodeType.AUTHOR],
   [RelationType.RETWEETS]: [NodeType.AUTHOR, NodeType.AUTHOR],
   [RelationType.SHARES_HASHTAG]: [NodeType.AUTHOR, NodeType.AUTHOR],
   [RelationType.REPLIES]: [NodeType.AUTHOR, NodeType.AUTHOR],
-
-  // AUTHOR -> TWEET
   [RelationType.POSTED]: [NodeType.AUTHOR, NodeType.TWEET],
-
-  // AUTHOR -> HASHTAG
   [RelationType.USES_HASHTAG]: [NodeType.AUTHOR, NodeType.HASHTAG],
-
-  // TWEET -> TWEET relations
   [RelationType.HAS_PARENT]: [NodeType.TWEET, NodeType.TWEET],
   [RelationType.RETWEETED]: [NodeType.TWEET, NodeType.TWEET],
   [RelationType.QUOTED]: [NodeType.TWEET, NodeType.TWEET],
   [RelationType.REPLY_TO]: [NodeType.TWEET, NodeType.TWEET],
-
-  // TWEET -> AUTHOR
   [RelationType.MENTION]: [NodeType.TWEET, NodeType.AUTHOR],
   [RelationType.HAS_REPLY]: [NodeType.TWEET, NodeType.AUTHOR],
-
-  // TWEET -> HASHTAG
   [RelationType.HAS_HASHTAG]: [NodeType.TWEET, NodeType.HASHTAG],
 };
 
