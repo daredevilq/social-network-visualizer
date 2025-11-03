291d1819e83b543828cd79e0d6c786996a024590
Wiktor Dybalski <wdybalski@student.agh.edu.pl>
2025-11-04 00:19:15 +0100

    remove curvature

--- social-network-visualizer-frontend/src/app/graph/BaseGraph.tsx
+++ social-network-visualizer-frontend/src/app/graph/BaseGraph.tsx
@@ -113,7 +113,6 @@ const BaseGraph = forwardRef((props: GraphProps, ref) => {
       .linkLabel(linkLabel)
       .linkDirectionalArrowLength(linkDirectionalArrowLength)
       .linkDirectionalArrowRelPos(linkDirectionalArrowRelPos)
-      .linkCurvature(Math.random() - 0.5)
       .onNodeClick(handleNodeLeftClick)
       .onNodeRightClick(handleNodeRightClick)
       .nodeCanvasObject((node: GraphNode & { x: number; y: number }, ctx: CanvasRenderingContext2D, globalScale: any) => {
