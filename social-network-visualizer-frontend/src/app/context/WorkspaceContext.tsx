06155413aa755baff252fb8762e16e6b2266dad7
Tomasz Furgała <tomaszfurgala23@gmail.com>
2025-11-16 23:43:06 +0100

    Add some UI improvements (#59)
    * Update sidebars' styles
    
    * add closing right sidebar on get back to graph
    
    * fix banners styles

--- social-network-visualizer-frontend/src/app/context/WorkspaceContext.tsx
+++ social-network-visualizer-frontend/src/app/context/WorkspaceContext.tsx
@@ -61,7 +61,8 @@ export const WorkspaceProvider: React.FC<{ children: React.ReactNode }> = ({ chi
     links: GraphLink[];
   }>({ nodes: [], links: [] });
   const { showNotification } = useNotification();
-  const { runWithLoading, loadedProjectName } = useProject();
+  const { runWithLoading, loadedProjectName, setIsSidebarOpen, setNodeFound, setShortestPath, setFocusedCommunityId, setSelectedUserData } =
+    useProject();
   const { saveCurrentGraphData } = useSaveWorkspaceChanges();
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
   const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
@@ -72,6 +73,15 @@ export const WorkspaceProvider: React.FC<{ children: React.ReactNode }> = ({ chi
     links: GraphLink[];
   }>({ nodes: [], links: [] });
   const [workspaces, setWorkspaces] = useState<string[]>([]);
+
+  useEffect(() => {
+    setIsSidebarOpen(false);
+    setNodeFound(null);
+    setShortestPath([]);
+    setFocusedCommunityId(undefined);
+    setSelectedUserData(null);
+    setHasUnsavedChanges(false);
+  }, [openedWorkspaceName, loadedProjectName]);
 
   useEffect(() => {
     fetchWorkspaceData();
--- social-network-visualizer-frontend/src/app/graph/BaseGraph.tsx
+++ social-network-visualizer-frontend/src/app/graph/BaseGraph.tsx
@@ -82,6 +82,7 @@ const BaseGraph = forwardRef((props: GraphProps, ref) => {
   }, []);
 
   useEffect(() => {
+    setIsSidebarOpen(false);
     if (!containerRef.current) return;
 
     fgInstance.current = new ForceGraph<GraphNode, GraphLink>(containerRef.current);
