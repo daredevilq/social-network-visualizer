58b127148adf57d8c037fa3bcb01dbf320ba239d
daredevilq <110993908+daredevilq@users.noreply.github.com>
2025-12-16 16:17:35 +0100

    Moved favicon.ico to public directory and updated layout.tsx to reference new location

--- social-network-visualizer-frontend/src/app/layout.tsx
+++ social-network-visualizer-frontend/src/app/layout.tsx
@@ -6,6 +6,9 @@ import { AppProviders } from '@/app/providers/AppComposers';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body suppressHydrationWarning>
         <AppProviders>{children}</AppProviders>
       </body>
